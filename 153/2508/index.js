8811556e0ad0281625f5bd1bab4133e05c1562d6
queckezz <fabian.eichenberger@gmail.com>
2016-09-20 10:43:58 +0200

    update generate script to insert at once

--- 153/2508/index.js
+++ 153/2508/index.js
@@ -4,8 +4,11 @@ const { resolve, join } = require('path')
 
 const fullPath = join(process.cwd(), 'generate.sql')
 
-const inserts = Array.from(Array(100000).keys()).map((_, i) => {
-  return `INSERT into benchmarktable (description, number) values ('test', ${i});`
-})
+const inserts = Array.from(Array(100000).keys())
+  .map((_, i) => `  ('test', ${i})`)
 
-writeFileSync(fullPath, inserts.join('\n'))
+writeFileSync(fullPath, `
+use benchmark;
+insert into benchmarktable (description, number) values
+${inserts.join(',\n')};
+`)
---
+++ 153/2508/solution.md
@@ -0,0 +1,10 @@
+
+# Was ist schneller: Eine Query mit where auf „id“ oder auf „number“?
+
+# Wie ist es, nachdem Sie einen Primary Key einfügen?
+
+# Ein LIKE mit % zu Beginn oder % am Schluss? Mit oder ohne Index?
+
+# Mit oder ohne Index auf „number“?
+
+# Mit und ohne Index bei „order by“?
